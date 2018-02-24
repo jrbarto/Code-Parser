f9015bf39016c2fe1618f647308aba304be080b9
jrbarto <jrbarto@us.ibm.com>
2018-02-23 23:04:09 -0500

    Updated Driver class to output line numbers in correct format.

--- src/main/java/com/codeparser/Driver.java
+++ src/main/java/com/codeparser/Driver.java
@@ -7,23 +7,22 @@ package com.codeparser;
 
 import javax.swing.SwingUtilities;
 public class Driver{
-	
+
 	public Driver() {}
 
-	
+
 	public static void m2ManyArgs(Bracket rack, String lines[], int max){
 		String l[] = rack.listMethods();
 		for(int i = 0, totalArgs; i < l.length; i++){
 			totalArgs = bCodeParser.totalArgsInMethod(l[i]);
 			if (totalArgs > max){
-				System.out.print(l[i] + " totalargs = " + totalArgs);
-				System.out.println(" LineNumber = " + bCodeParser.methodLineNumber(l[i], lines));
+				System.out.println(bCodeParser.methodLineNumber(l[i], lines));
 			}
 		}
 	}
-	
+
 	public static void mTooManyArgs(String filename, int max){
-		//Bracket removes comments from source. Therefore, the line numbers are changed and can't be used. 
+		//Bracket removes comments from source. Therefore, the line numbers are changed and can't be used.
 		Bracket rack = new Bracket(filename);
 		//Note: bCodeParser.fileToLines() should not be looped if the filename is not changed in the loop
 		//This will force the program to read from file multiple times (exponentially slower)
@@ -33,36 +32,35 @@ public class Driver{
 		for(int i = 0, totalArgs; i < l.length; i++){
 			totalArgs = bCodeParser.totalArgsInMethod(l[i]);
 			if (totalArgs > max){
-				System.out.print(l[i] + " totalargs = " + totalArgs);
-				System.out.println(" LineNumber = " + bCodeParser.methodLineNumber(l[i], lines));
+				System.out.println(bCodeParser.methodLineNumber(l[i], lines));
 			}
 		}
 	}
-	
+
 	public static void printGlobal(String filename) {
 		//Filename should be checked for null before calling this method
 		Bracket rack = new Bracket(filename);
 		rack.print();
 	}
-	
+
 	public static void displayGlobal(String filename){
 		//Filename should be checked for null before calling this method
 		Bracket rack = new Bracket(filename);
 		String lines[] = bCodeParser.fileToLines(filename);
 		//rack.print();
 		SwingUtilities.invokeLater(new Runnable() {
-            @Override
-            public void run() {
-        		new bCodeViewer(rack.getJTree(lines));
-            }
-        });
+			@Override
+			public void run() {
+				new bCodeViewer(rack.getJTree(lines));
+			}
+		});
 		//m2ManyArgs(rack, lines, 1);
 	}
-	
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
-		//I'm using eclipse to debug. Therefore, there are no args supplied to main(String[])  
+
+		//I'm using eclipse to debug. Therefore, there are no args supplied to main(String[])
 		/*if (args.length < 1) {
 			System.out.println("[Error] You must specify a file argument to parse.");
 			System.exit(1);
